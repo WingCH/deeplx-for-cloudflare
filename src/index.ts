1135ffd7cfdb4d254c7ca41b46061ed0b9bd57e7
WingCH <wingch.apps@gmail.com>
2023-11-14 20:49:21 +0800

    [feat] add 簡轉繁

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
-import { Hono } from 'hono'
-import { query } from '@ifyour/deeplx'
+import {Hono} from 'hono'
+import {query} from '@ifyour/deeplx'
+import * as OpenCC from 'opencc-js';
 
 const app = new Hono()
 
 app
-  .get('/', (c) => c.redirect('/translate'))
-  .get('/translate', (c) => c.text('Please use POST method :)'))
-  .post('/translate', async c => {
-    const params = await c.req.json().catch(() => ({}))
-    const result = await query(params, { proxyEndpoint: 'https://ideepl.vercel.app/jsonrpc' })
-    return c.json(result, result.code)
-  })
+    .get('/', (c) => c.redirect('/translate'))
+    .get('/translate', (c) => c.text('Please use POST method :)'))
+    .post('/translate', async c => {
+        const params = await c.req.json().catch(() => ({}))
+        const result = await query(params, {proxyEndpoint: 'https://ideepl.vercel.app/jsonrpc'})
+        if (result.code === 200 && result.target_lang === "ZH" && result.data) {
+            const converter = OpenCC.Converter({from: 'cn', to: 'hk'})
+            const data = result.data
+            result.data = converter(data)
+        }
+        return c.json(result, result.code)
+    })
 
 export default app
---
+++ src/opencc-js.d.ts
@@ -0,0 +1,4 @@
+declare module 'opencc-js' {
+    const content: any;
+    export = content;
+}
